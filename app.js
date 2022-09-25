68642d40f5780ce4b50043c3bed678e1248a6b5f
Züleyha Kurt <netice2017@gmail.com>
2022-09-25 22:59:17 +0300

    edit

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const buton = document.querySelector(".btn")
 const main = document.querySelector(".content")
 let name1;
 let nameC;
+let counter = 0;
 
 const fetchCity = (name) => {
+    if (counter > 4) {
+        alert("maximum entry is 5")
+    }
+    else{
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=8679780ea9ee4fcdc4514f93f08f4e3f`
 
     fetch(url)
@@ -19,7 +24,7 @@ const fetchCity = (name) => {
     })
     .then((data) => renderCity(data))
     .catch((err) => console.log(err));
-
+    }
 
 }
 const renderError = () => {
@@ -60,7 +65,9 @@ const renderCity = (data) => {
     buton.addEventListener("click", () => {
         let inputCity = input.value
         fetchCity(inputCity)
-        input.value=""
+        input.value = ""
+        counter++
+
     })
 
     window.onload = function () {
